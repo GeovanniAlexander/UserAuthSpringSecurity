00e8dacd0639a441e0a747689a9fd9d591409255
Alex <alex123joha@gmail.com>
2021-08-31 10:01:14 -0500

    Setup app

---
+++ src/main/java/com/example/authuserspringsecurity/AuthUserSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.authuserspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUserSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authuserspringsecurity/AuthUserSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authuserspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUserSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
